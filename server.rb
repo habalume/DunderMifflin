a09aa2e00406730463d42c8aa27a560b698c19b0
Katy Pola <katyp03@gmail.com>
2017-06-27 10:14:23 -0400

    Starting ordering form functionality

--- server.rb
+++ server.rb
@@ -16,6 +16,14 @@ get '/ordering' do
 	erb :ordering
 end
 
+# for ordering form:
+# post '/ordering' do
+# 	p params
+# 	user = params[:name]
+# 	address = params[:email]
+# 	redirect '/ordering'
+# end	
+
 get '/contact' do
   erb :contact
 end
@@ -29,7 +37,6 @@ post '/contact' do
 	subject = params[:subject]
 	content = Content.new(type: 'text/plain', value: params[:body])
 	mail = Mail.new(from, subject, to, content)
-
 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 	response = sg.client.mail._('send').post(request_body: mail.to_json)
 	puts response.status_code
