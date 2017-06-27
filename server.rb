7f01f23ce7300a3b3101dc67e0ed74fdd6d04a77
Katy Pola <katyp03@gmail.com>
2017-06-27 16:59:40 -0400

    Pushing more pictures to git and attempting functionality for order form

--- server.rb
+++ server.rb
@@ -18,12 +18,20 @@ get '/ordering' do
 end
 
 # for ordering form:
-# post '/ordering' do
-# 	p params
-# 	user = params[:name]
-# 	address = params[:email]
-# 	redirect '/ordering'
-# end	
+post '/ordering' do
+	p params
+	to = Email.new(email: params[:email])
+	from = Email.new(email: 'katypola03@yahoo.com')
+	subject = 'Your order is being processed!'
+	content = Content.new(type: 'text/plain', value: 'Thanks, ' + params[:name] + '! Your order is currently being processed by our fantastic warehouse team.  You ordered ' + params[:cases] + ' and for shipping, you chose ' + params[:shipping] +'.  You will receive another email when your order is on the way!')
+	mail = Mail.new(from, subject, to, content)
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+	redirect '/ordering'
+end	
 
 get '/contact' do
   erb :contact
